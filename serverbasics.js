0c1c2c50475ae114194b40855c3303590d4d1c12
Holly <hollylinnn@yahoo.com>
2016-04-02 19:53:51 +0000

    serverbasics

---
+++ serverbasics.js
@@ -0,0 +1,15 @@
+// cd . && npm install (at bash to change it to express)
+
+var express = require('express');
+var app = express ();
+
+app.get('/', function (req, res) {
+    res.send('Welcome!');
+});
+app.get('/bye', function(req, res) {
+    res.send('Goodbye!');
+});
+app.get()
+app.listen(process.env.PORT, function () {
+    console.log('Example app listening on  port 3000!');
+});
