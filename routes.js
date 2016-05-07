6569fb0995a1b11d681d72fdeee44f5ba8c38c32
Holly <hollylinnn@yahoo.com>
2016-05-07 17:13:07 +0000

    update to class 18

--- routes.js
+++ routes.js
@@ -1,20 +1,21 @@
-// Local deps 
+// Local dependencies
 var controllers = require('./controllers');
 
-var Router = { 
-    
+var Router = {
+
     init: function(app) {
-        
-        // Home page 
+
+        // Home page
         app.get('/', controllers.Home.index);
-        
+
         // About page
         app.get('/about', controllers.About.index);
-        
-        // Users page
+
+        // Users
         app.get('/users', controllers.Users.index);
+
     }
-    
+
 };
 
-module.exports = Router; 
+module.exports = Router;
